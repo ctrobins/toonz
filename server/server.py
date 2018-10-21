924070ffa4ec6210314c9dd0edcf8f8d06ecd847
Carter Robinson <cartertrobinson@gmail.com>
2018-10-21 11:25:49 -0500

    Successful Spotify Oauth

--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, session, request, redirect, render_template
 import spotipy
+import spotipy.oauth2 as oauth2
 import spotipy.util as util
 
 app = Flask(__name__, static_folder='../build/static', template_folder='../build')
 app.secret_key = 'RocketDollar'
 
+SCOPE='playlist-modify-public playlist-modify-private playlist-read-private user-top-read user-library-read'
 CLIENT_ID='baa8b0e6a0cf4aaaa442b5b11a343660'
 CLIENT_SECRET='10afa836a55c4df0a3187f75e518ae55'
 REDIRECT_URI='http://localhost:5000/callback/'
@@ -14,23 +16,27 @@ def react_app():
 
 @app.route('/api/data')
 def data():
-    return session.get('token')
+   return 'hello'
 
 @app.route('/callback/')
 def callback():
-    auth_token = request.args['code']
-    session['token'] = auth_token
+    url = request.url
+    sp_oauth = oauth2.SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, SCOPE)
+    code = sp_oauth.parse_response_code(url)
+    token_info = sp_oauth.get_access_token(code)
+    session['token'] = token_info['access_token']
     return redirect("/", code=302)
 
 @app.route('/login', methods=['POST'])
 def authenticate_user():
     username = request.json['username']
-    util.prompt_for_user_token(username, 
-        'user-library-read', 
+    token = util.prompt_for_user_token(username, 
+        SCOPE,
         CLIENT_ID, 
         CLIENT_SECRET,
         REDIRECT_URI)
-    return 'authenticated'
+    session['token'] = token
+    return redirect("/", code=302)
 
 @app.route('/logout')
 def logout_user():
@@ -48,13 +54,22 @@ def signup_screen():
 
 @app.route('/')
 def check_auth():
-    if 'token' not in session:
-        return redirect("/login", code=302)
-    else:
-        return react_app()
+        #session.clear()
+        for el in session:
+                print(el)
+        if 'token' not in session:
+                return redirect("/login", code=302)
+        else:
+                print('TOKEN', session.get('token'))
+                sp = spotipy.Spotify(session.get('token'))
+                sp.trace = False
+                results = sp.current_user_saved_tracks()
+                for item in results['items']:
+                        track = item['track']
+                        print track['name'] + ' - ' + track['artists'][0]['name']
+                return react_app()
 
 # Default case to rereoute other URLs to React Router
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/<path>')
 def handle_route(path):
     return redirect('/', code=302)
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import spotifyApi from 'spotify-web-api-node';
 
 class Playlist extends Component {
     constructor(props) {
@@ -7,13 +8,26 @@ class Playlist extends Component {
         this.state = {
             token: ''
         };
+        this.spotify = new spotifyApi();
     }
 
     componentDidMount() {
-      axios.get(`/api/data`)
-        .then(res => res.data) 
-        .then(token => this.setState({ token }))
-        .catch(console.log);
+        this.spotify.getAlbums(['5U4W9E5WsYb2jUQWePT8Xm', '3KyVcddATClQKIdtaap4bV'])
+        .then(function(data) {
+          console.log('Albums information', data.body);
+        }, function(err) {
+          console.error(err);
+        });
+    //   axios.get(`/api/data`)
+    //     .then(res => res.data) 
+    //     //.then(token => this.setState({ token }))
+    //     .then(token => this.spotify.setAccessToken(token))
+    //     .then(() => {
+    //         this.spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+    //             .then(console.log)
+    //             .catch(console.log)
+    //     })
+    //     .catch(console.log);
     }
 
     render() {
